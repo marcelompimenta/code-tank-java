b44b1f0130675d1772f50638d27dd7bee8c08ae4
marcelo pimenta <marcelopimenta.work@gmail.com>
2022-06-26 14:33:19 -0400

    first commit with java language

---
+++ src/test.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+public class test {
+    public static void main(String args[]){
+        System.out.println("Teste inicial de Java");
+        JOptionPane.showInputDialog(null, "Enter your password.");
+        JOptionPane.showConfirmDialog(null, "this password is correct?");
+    }
+}
